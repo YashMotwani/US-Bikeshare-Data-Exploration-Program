e9f9c1656335379e7fd59b57e190c2c4d44d97ef
Yash Motwani <yashmotwani2909@gmail.com>
2018-12-14 03:25:41 +0530

    fix: Key Error correction complete
    Key Error corection for 'washington' in previous commit got completed

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -170,7 +170,7 @@ def main():
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
-        user_stats(df)
+        user_stats(df, city)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
